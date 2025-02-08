0d2f8fd3d8611aff2f5ef0da43929946dadd5f66
syedshara <syedsharashree@gmail.com>
2025-02-08 14:31:12 +0530

    Deploying to Render

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,14 +2,15 @@ import { motion } from "framer-motion";
 import Navigation from "../components/Navigation";
 import Sec1 from "../components/sections/Sec1";
 import Sec2 from "../components/sections/Sec2";
+import img from "../assets/bg.jpg";
 
 export default function Portfolio() {
-    const url = "https://img.freepik.com/free-vector/black-geometric-memphis-social-banner-vector_53876-157880.jpg?t=st=1738948192~exp=1738951792~hmac=be5836cf344a0056ad1b72b553dda5da39bbe3748c6b4350e257371788883a6b&w=1800";
+
 
 
     return (
         <div className="relative min-h-screen bg-slate-200">
-            <div className="h-[65vh] bg-cover bg-center pt-4 relative z-0" style={{ backgroundImage: `url(${url})` }} >
+            <div className="h-[65vh] bg-cover bg-center pt-4 relative z-0" style={{ backgroundImage: `url(${img})` }} >
                 <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
                 <Navigation />
                 <header className="w-full text-center py-32 text-white relative z-20">
